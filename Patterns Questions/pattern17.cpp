8b37391da6f427b659c48795cef415002a170813
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-03-21 10:26:29 +0530

    More Pattern questions

---
+++ Patterns Questions/pattern17.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int i = 1;
+    while (i<=n)
+    {
+        int space = n-i;
+        while (space)
+        {
+            cout<<" ";
+            space -= 1;
+        }
+        int j =1;
+        while (j<=i)
+        {
+            cout<<j;
+            j++;
+        }
+        int k = i-1;
+        while (k)
+        {
+            cout<<k;
+            k--;
+        }
+        
+        cout<<endl;
+        i++;
+        
+       
+
+    }
+    
+
+
+}
