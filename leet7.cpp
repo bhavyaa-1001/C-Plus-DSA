09843416891af3c0a4a488be5ced036c9530025d
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-06-25 23:11:18 +0530

    leet

--- leet7.cpp
+++ leet7.cpp
@@ -7,9 +7,18 @@ int main(){
     int n;
     cin >> n ;
     int ans = 0;
+    int INT_MAX ;
+    int INT_MIN ;
     while (n!=0)
     {
         int digit = n%10;
+
+        if ((ans > INT_MAX/10) || (ans < INT_MIN/10))
+        {
+            return 0;
+        }
+        
+
         ans = ans *10 + digit;
         n = n/10;
     }
