181b5c6d4b87b3c644d6d793487a90438769101f
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-08-08 03:04:05 +0530

    VECTOR

--- STL LB/vector.cxx
+++ STL LB/vector.cxx
@@ -1,5 +1,14 @@
 #include <iostream>
-#include <array>
+#include <vector>
 using namespace std;
 
-int main()
+int main(){
+
+    vector<int> v;
+    cout << "Size of vector: " << v.size() << endl;
+
+    v.push_back(10);
+    cout << "Size of vector after push_back: " << v.size() << endl;
+    cout << "Capacity of vector: " << v.capacity() << endl;
+
+}
