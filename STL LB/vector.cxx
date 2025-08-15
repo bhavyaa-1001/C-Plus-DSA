5e83bc7622f4a5d599e383ed0b893c5e4610aba2
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-08-15 12:47:59 +0530

    vector

--- STL LB/vector.cxx
+++ STL LB/vector.cxx
@@ -1,14 +1,51 @@
 #include <iostream>
-#include <vector>
+#include<vector>
 using namespace std;
 
-int main(){
+int main() {
 
-    vector<int> v;
-    cout << "Size of vector: " << v.size() << endl;
+vector<int> a(5,1);
 
-    v.push_back(10);
-    cout << "Size of vector after push_back: " << v.size() << endl;
-    cout << "Capacity of vector: " << v.capacity() << endl;
+ for (int j : a) {
+        cout << j << " ";
+    }cout<<endl;
+
+vector<int> v;
+
+cout<<"Capacity:"<<v.capacity()<<endl;
+
+v.push_back(1);
+cout<<"Capacity:"<<v.capacity()<<endl;
+
+v.push_back(2);
+cout<<"Capacity:"<<v.capacity()<<endl;
+
+v.push_back(3);
+cout<<"Capacity:"<<v.capacity()<<endl;
+
+v.push_back(7);
+cout<<"Capacity:"<<v.capacity()<<endl;
+
+cout<<"Size:"<<v.size()<<endl;
+
+cout<<"Element at 2nd index is:"<<v.at(3)<<endl;
+
+cout<<"Front:"<<v.front()<<endl;
+cout<<"Back:"<<v.back()<<endl;
+
+cout<<"Before Pop"<<endl;
+for(int i:v){
+    cout<<i<<"";
+}cout<<endl;
+
+v.pop_back();
+
+cout<<"After Pop"<<endl;
+for(int i:v){
+    cout<<i<<"";
+}cout<<endl;
+
+
+
 
 }
