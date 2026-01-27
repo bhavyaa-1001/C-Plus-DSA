8351a51ddbe17f89b16b0df3ccebcdc7df4a063a
bhavyaa-1001 <bhavyyaa106@gmail.com>
2026-01-27 23:22:04 +0530

    Row wise sum in 2D Array

---
+++ L-23_2DArrays/RowWiseSum.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main(){
+
+    int arr[3][4];
+
+    // taking input in 2D array
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> arr[i][j];
+        }
+        
+    }
+
+    // print 2D array
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+        
+    }
+    // row wise sum
+    for (int i = 0; i < 3; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            sum += arr[i][j];
+        }
+        cout << "Sum of row " << i << " is " << sum << endl;
+        
+    }
+}
