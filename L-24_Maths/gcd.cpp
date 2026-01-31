387844e7039c8db015b9018d9799913ca1eedbeb
bhavyaa-1001 <bhavyyaa106@gmail.com>
2026-02-01 00:38:35 +0530

    GCD

---
+++ L-24_Maths/gcd.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b){
+    if (b == 0)
+        return a;
+    if (a == 0)
+        return b;
+    
+    while(a != b){
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+    return a;
+}
+
+int main(){
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "GCD of " << a << " and " << b << " is " << gcd(a, b) << endl;
+
+    return 0;
+}
