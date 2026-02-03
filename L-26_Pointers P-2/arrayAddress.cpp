4bc34182dbc866ee0f5a1f46c4443cb94f4cb034
bhavyaa-1001 <bhavyyaa106@gmail.com>
2026-02-04 01:06:51 +0530

    arra6y pointers

--- L-26_Pointers P-2/arrayAddress.cpp
+++ L-26_Pointers P-2/arrayAddress.cpp
@@ -14,10 +14,14 @@ int main() {
     cout << "Value of ptr (address of first element): " << ptr << endl;
     cout << "Value pointed to by ptr: " << *ptr << endl;
 
+
     // Accessing other elements using pointer arithmetic
     for (int i = 0; i < 5; i++) {
         cout << "Element " << i << ": " << *(ptr + i) << " at address " << (ptr + i) << endl;
     }
+
+    cout << *arr << endl; // This will print the first element of the array (10)
+    cout << *(arr + 1) << endl; // This will print the second element of the array (20)
 
     return 0;
 }
