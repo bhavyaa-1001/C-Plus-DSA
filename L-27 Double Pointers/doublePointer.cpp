7ce4b2836f9f3586351de7ea1cac876474dbc4d7
bhavyaa-1001 <bhavyyaa106@gmail.com>
2026-02-04 15:53:00 +0530

    Double Pointer

---
+++ L-27 Double Pointers/doublePointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int i = 5;
+    int *p = &i;
+    int **pp = &p;
+    cout << "Value of i: " << i << endl;
+    cout << "Value via single pointer p: " << *p << endl;
+    cout << "Value via double pointer pp: " << **pp << endl;
+    cout << "Address of i: " << &i << endl;
+    cout << "Address via single pointer p: " << p << endl;
+    cout << "Address via double pointer pp: " << *pp << endl;
+
+    cout << &p << endl;
+    cout << "Address of double pointer pp: " << pp << endl;
+    cout << "Address of double pointer pp: " << &pp << endl;
+
+
+    return 0;
+}
