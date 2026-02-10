c64df22b6a4f140f3621df0eaab89af3cfd95b47
bhavyaa-1001 <bhavyyaa106@gmail.com>
2026-02-10 17:53:05 +0530

    dynamic Array

---
+++ L-28 Reference variable/dynamicArrray.cxx
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int getSum(int *arr, int n) {
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    int *arr = new int[n]; // Dynamic array allocation
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i]; // Input values into the array
+    }
+    int ans = getSum(arr, n);
+    cout << "Sum of the array is: " << ans << endl;
+}
