48b3b019d62ed08afd09242bb6b9cc02442e607a
bhavyaa-1001 <bhavyyaa106@gmail.com>
2026-02-17 02:59:03 +0530

    recursion binary search

---
+++ L-33 Recursion Part3/BInarySearch.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+bool BinarySearch(int arr[], int s,int e, int key){
+    if(s > e){
+        return false;
+    }
+    int mid = s + (e-s)/2;
+    if(arr[mid] == key){
+        return true;
+    }
+    else if(arr[mid] > key){
+        return BinarySearch(arr, s, mid-1, key);
+    }
+    else{
+        return BinarySearch(arr, mid+1, e, key);
+    }
+}
+
+int main(){
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int key = 4;
+    cout << "Key " << key << "  " << BinarySearch(arr, 0, n-1, key) << endl;
+
+}
