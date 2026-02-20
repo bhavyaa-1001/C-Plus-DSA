d267c65587d40e962c29cd138c52418a735bb06d
bhavyaa-1001 <bhavyyaa106@gmail.com>
2026-02-20 21:18:12 +0530

    recursion quick sort

--- L-34 Recursion Part 4/quickSort.cpp
+++ L-34 Recursion Part 4/quickSort.cpp
@@ -32,9 +32,7 @@ void quickSort(int arr[], int s, int e){
     if (s>= e){
         return;
     }
-
     int p = partition(arr, s, e);
-
     quickSort(arr, s, p-1);
     quickSort(arr, p+1, e);
 }
