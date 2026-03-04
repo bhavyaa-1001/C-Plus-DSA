e57fcd52c8ad560733bb847ece6a4bca69f1e5fd
bhavyaa-1001 <bhavyyaa106@gmail.com>
2026-03-04 20:53:39 +0530

    Code Chef Contest P1

--- Code_Chef/Start228D/BOP1.cpp
+++ Code_Chef/Start228D/BOP1.cpp
@@ -1,3 +1,36 @@
+// Balloon Splash
+
+// Alice and Bob are playing a balloon splash game.
+
+// Alice has XX water balloons and Bob has YY water balloons.
+
+// The player with more balloons wins. If both players have the same number of balloons, the result is a draw.
+
+// Output:
+
+//     Alice if Alice wins
+//     Bob if Bob wins
+//     Draw if it is a draw
+
+// Input Format
+
+//     The first line contains two space-separated integers XX and YY.
+
+// Output Format
+
+// Output a single string denoting the result of game:
+
+//     Alice if Alice wins
+//     Bob if Bob wins
+//     Draw if it is a draw
+
+// Each character can be printed in either uppercase or lowercase, i.e. if the winner is Bob, the outputs BOB, Bob, bOb, and so on will all be accepted.
+// Constraints
+
+//     1≤X,Y≤1001≤X,Y≤100
+
+
+
 #include<iostream>
 using namespace std;
 
