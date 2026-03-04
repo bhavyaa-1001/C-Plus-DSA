2d28ea75f1d0b9f22a8127da3c7e0c06d3a8b4a5
bhavyaa-1001 <bhavyyaa106@gmail.com>
2026-03-04 20:53:06 +0530

    Code Chef Contest P2

--- Code_Chef/Start228D/BOP2.cpp
+++ Code_Chef/Start228D/BOP2.cpp
@@ -1,3 +1,35 @@
+// Color Festival
+
+// It is the festival of colors, and you decide to visit your NN friends one by one.
+
+// When you visit friend ii, your face is painted with color CiCi​.
+
+//     Initially, your face has no color.
+//     If the color currently on your face is different from the color applied by the next friend, your face color changes.
+//     In particular, when your face has no color and a color is applied to it, your face will change color.
+
+// Each time your face color changes, you feel a jolt.
+
+// You are allowed to choose the order in which you visit your friends, but you must visit every one of them exactly once each.
+// Find the minimum possible number of jolts.
+// Input Format
+
+//     The first line contains an integer TT — the number of test cases.
+//     For each test case:
+//         The first line contains an integer NN — the number of friends.
+//         The second line contains NN integers C1,C2,…,CNC1​,C2​,…,CN​ — where CiCi​ is the color painted by friend ii.
+
+// Output Format
+
+// For each test case, output a single integer — the minimum possible number of jolts.
+// Constraints
+
+//     1≤T≤1001≤T≤100
+//     1≤N≤1001≤N≤100
+//     1≤Ci≤N1≤Ci​≤N
+
+
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
