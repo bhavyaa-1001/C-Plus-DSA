067b96914cfb01ca2820a37d8561768a38a15b6b
bhavyaa-1001 <bhavyyaa106@gmail.com>
2026-03-08 16:55:51 +0530

    lc 1980

---
+++ leetcode/leet1980.cpp/leet.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+        string ans = "";
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i][i] == '0')
+                ans += '1';
+            else
+                ans += '0';
+        }
+
+        return ans;
+    }
+};
